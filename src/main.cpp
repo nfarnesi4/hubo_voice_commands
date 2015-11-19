9065f1aa426eff0e22e15358724efcd61261ed6d
Reno <nfarnesi4@gmail.com>
2015-11-19 00:14:38 -0500

    Cleaned the code. Added a lot more comments. Added functions for checking for special commands like keywords and exit commands. Edited the grammar file alot. Got rid of all one word commands because they create a lot of false positves. ok hubo is now gone because it had too many false positves. Added two new keywords. Added new constant for hyp score threshold and for a max number of atempts for recieving a command.

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,47 @@
 //#define MODELDIRECTORY "/usr/local/share/pocketsphinx/model/hubo-us/"
 
 //consts:
+
+//the dirs for all the resources
 const char* grammarLocation = MODELDIRECTORY "hubo.jsgf";
 const char* keywordsLocation = MODELDIRECTORY "keywords.txt";
 const char* lmLocation = MODELDIRECTORY "en-us.lm.bin";
 const char* dictLocation = MODELDIRECTORY "cmudict-en-us.dict";
+const char* hmmLocation = MODELDIRECTORY "en-us";
 
-
+//the different search modes
 const char* keywordsSearch = "keyword";
 const char* grammarSearch = "commands";
 const char* lmSearch = "commandslm";
- 
+
 const unsigned int kSleepTime = 100000; //sleep time is us
 
-//const int32 kSampleRate = 16000; //samples per second 
+//const int32 kSampleRate = 16000; //samples per second
+
+// the min score that will be accepted for the grammar search
+const int32 kScoreThres = -5000;
+
+// the max number of times grammar search will be attempted before
+// the program switches back to keyword search
+const unsigned int kMaxGrammarSearchTries = 4;
 
 const unsigned int kBufferSize = 2048; //audio buffer size
 
 //function protos:
 
+//test if the input is one of the exit commands
+bool isExitCmd(const char*);
+
+//test if the input string is one of the keywords
+bool isKeyword(const char*);
+
+//run the command based on the input
 int runCommand(const char*);
 
+//init all the sphix stuff
 bool init();
 
+//free up the memory
 void close();
 
 //global objeccts:
@@ -44,20 +63,20 @@ cmd_ln_t *config;
 
 
 int main(int argc, char *argv[]){
-  
-
+  //hold the guess for what the person said
   char const *hyp;
-  
+  //the score for the guess of what the person said
   int32 score;
-
+  //booleans to hold the differnt states
   uint8 uttStarted, inSpeech, commandMode;
-
+  //true when the user exits the program
   bool endRec = false;
-
+  //buffer to hold the recorded audio
   int16 buff[kBufferSize];
-
+  //holds the value of what was read in from the audio device
   int32 adReadValue;
-
+  //stores the number of grammar search tries
+  unsigned int grammarSearchTries = 0;
 
   if(init()){
   
@@ -114,62 +133,130 @@ int main(int argc, char *argv[]){
       //get the result and store it in hyp
       hyp = ps_get_hyp(ps, &score);
 
+      //if were not looking for a command check for a keyword
       if(!commandMode){
+	//check for keyword
 	if(hyp != NULL){
-	  std::cout << "\nI heard: ok hubo" << std::endl;
+	  std::cout << "\nI heard: A keyword: " << hyp << std::endl;
+	  //keyword found start looking for a command
 	  commandMode = TRUE;
-	  ps_set_search(ps, lmSearch);
-	  std::cout << "Search switched to lm Search" << std::endl;
+	  ps_set_search(ps, grammarSearch);
+	  std::cout << "Search switched to grammar" << std::endl;
 	}
       }
       else{
-	if( hyp != NULL && (strcmp(hyp, "") != 0) ){
-	  std::cout << "\nI heard: " << hyp << " Score: " << score << std::endl;
-	  if( (strcmp(hyp, "ok hubo")) != 0 ){
-            if( (strcmp(hyp, "stop")) == 0 ){
-              endRec =  true;
-            }
-	    else{
-	      runCommand(hyp);
+	if(grammarSearchTries < kMaxGrammarSearchTries){
+	  if( hyp != NULL && (strcmp(hyp, "") != 0) ){
+	    std::cout << "\nI heard: " << hyp << " Score: " << score << std::endl;
+	    //check to make sure the score is good
+	    if(score > kScoreThres){
+	      //check to make sure its not a keyword again
+	      if( !isKeyword(hyp) ){
+		//check to see if its and exit command
+		if( isExitCmd(hyp) ){
+		  endRec =  true;
+		}
+		else{
+		  runCommand(hyp);
+		}
+		//everything went well go back to keyword search
+		commandMode = FALSE;
+		ps_set_search(ps, keywordsSearch);
+		grammarSearchTries = 0;
+		std::cout << "Looking for a keyword now" << std::endl;
+	      }
+	      else{
+		++grammarSearchTries;
+		std::cout << "The command is a keyword. Try: " << grammarSearchTries << std::endl;
+	      }
 	    }
-	    commandMode = FALSE;
-	    ps_set_search(ps, keywordsSearch);
-	    std::cout << "Looking for a keyword now" << std::endl;
-	  }       
+	    else{
+	      ++grammarSearchTries;
+	      std::cout << "Not above threshold. Try: " << grammarSearchTries << std::endl; 
+	    }  
+	  }
+	  else{
+	    //grammar could not be matched to anything
+	    ++grammarSearchTries;
+	    std::cout << "Could not find a command that matched the gram.";
+	    std::cout << " Try: " << grammarSearchTries << std::endl;
+	  }
+	}
+	else{
+	  //exceeded the number of tries
+	  //switch back to keyword search
+	  commandMode = FALSE;
+	  ps_set_search(ps, keywordsSearch);
+	  grammarSearchTries = 0;
+	  std::cout << "Max tries for grammar search reached. Switchig back to keyword" << std::endl;
 	}
       }
 
       //start a new utterance
       if(ps_start_utt(ps) < 0){
 	std::cout << "Failed to start utterance" << std::endl;
-	return -1;
       }
       uttStarted = FALSE;
       std::cout << "Ready" << std::endl;
     }
     //sleep for 100 ms
     usleep(kSleepTime);
-    
   }
 }
-
-    
+  //All done now free all the memory
   close();
   
   std::cout << "Bye" << std::endl;
   return 0;
 }
 
+//Functions:
+
+bool isExitCmd(const char* hyp){
+  //test for the exit commands
+  //this will be done from a file in the future
+  if (strncmp(hyp, "stop ", 5) == 0){
+    return true;
+  }
+  else if (strcmp(hyp, "close hubo") == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+}
+
+bool isKeyword(const char* hyp){
+  //test for all the keywords
+  //this will be done from the keywords file in the future
+
+  if (strcmp(hyp, "ok hubo") == 0){
+    return true;
+  }
+  else if (strcmp(hyp, "listen up hubo") == 0){
+    return true;
+  }
+  else if (strcmp(hyp, "what's up hubo") == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+}
 
 int runCommand(const char* command){
   int success = 0;
 
   //check for the different commands
-  if( strncmp(command, "wave", 4) == 0 )
+  //this will be done by reading from a file in the future
+  
+  if( strncmp(command, "wave to", 7) == 0 )
     success = system("python wave.py");
   else if( strncmp(command, "run dancing demo", 16) == 0 )
     success = system("python wave.py");
-  else if( strncmp(command, "run drumbing demo", 17) == 0 )
+  else if( strncmp(command, "run drumming demo", 17) == 0 )
     success = system("python wave.py");
   else if( strncmp(command, "run fabric demo", 15) == 0 )
     success = system("python wave.py");
@@ -193,16 +280,14 @@ int runCommand(const char* command){
 bool init(){
 
 config = cmd_ln_init(NULL, ps_args(), TRUE,
-		     "-hmm", MODELDIRECTORY "en-us",
+		     "-hmm", hmmLocation,
 		     "-lm", lmLocation,
 		     "-dict", dictLocation,
 		       NULL);
-  
 
-  if (config == NULL){
+ if (config == NULL){
     std::cout << "Failed to create config object, see log for details" << std::endl;
     return false;
-
   }
 
   ps = ps_init(config);
@@ -218,31 +303,26 @@ config = cmd_ln_init(NULL, ps_args(), TRUE,
     std::cout << "failed to add the keywords search" << std::endl;
     return false;
   }
-  
-  
+
   //add the gramar search to the ps
   if(ps_set_jsgf_file(ps, grammarSearch, grammarLocation) < 0){
     std::cout << "failed to add the grammer" << std::endl;
     return false;
   }
   
-  
   //add the commands language model
   if(ps_set_lm_file(ps, lmSearch, lmLocation) < 0){
     std::cout << "failed to add the language model" << std::endl;
     return false;
   }
-  
-  
-  
+
   //open the defualt audio device at the default sample rate
   if( (ad = ad_open()) == NULL ){
     std::cout << "\n!!!Device creation didn't work :( \n" << std::endl;
     //std::cout << "Failed to open audio device\n" << std::endl;
     return false;
   }
-
-
+  
   //everything set up correctly 
   return true;
 }
@@ -255,3 +335,4 @@ void close(){
   cmd_ln_free_r(config);
  
 }
+
